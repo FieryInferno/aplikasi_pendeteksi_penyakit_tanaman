a2b7fde6d57762f4e7b31b12639906acf634d7e8
FieryInferno <bagassetia271@gmail.com>
2023-05-25 10:34:49 +0700

    Fix blog page

--- lib/pages/blog_list.dart
+++ lib/pages/blog_list.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import '/pages/blog_detail.dart';
 import '/data/blogs.dart';
-import '/components/text_widget.dart';
 import '/components/click_widget.dart';
 import '/components/blog_item.dart';
-import '/components/menu.dart';
+import '/components/page_wrap.dart';
 
 class BlogList extends StatelessWidget {
   final List<Map<String, String>> _blogs = Blogs().list;
@@ -13,46 +12,15 @@ class BlogList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Container(
-              color: const Color(0xFFE7F0EB),
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Image.asset('assets/images/logo.png'),
-                      const SizedBox(width: 10),
-                      const TextWidget(
-                        'Blog',
-                        weight: FontWeight.bold,
-                        size: 24,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Expanded(
-                    child: ListView.separated(
-                      itemBuilder: (context, index) => ClickWidget(
-                        destination: BlogDetail(_blogs[index]),
-                        child: BlogItem(blog: _blogs[index]),
-                      ),
-                      separatorBuilder: (context, index) =>
-                          const SizedBox(height: 5),
-                      itemCount: _blogs.length,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const Menu(),
-          ],
+    return PageWrapper(
+      'Blog',
+      child: ListView.separated(
+        itemBuilder: (context, index) => ClickWidget(
+          destination: BlogDetail(_blogs[index]),
+          child: BlogItem(blog: _blogs[index]),
         ),
+        separatorBuilder: (context, index) => const SizedBox(height: 5),
+        itemCount: _blogs.length,
       ),
     );
   }
