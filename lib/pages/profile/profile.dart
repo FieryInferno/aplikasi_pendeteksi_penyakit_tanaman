b54ed824c39ac5c9bb97b86941b8da0b79aa57be
FieryInferno <bagassetia271@gmail.com>
2023-06-13 16:47:34 +0700

    Logout

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -1,10 +1,12 @@
 import 'package:aplikasi_pendeteksi_penyakit_tanaman/components/text_widget.dart';
 import 'package:aplikasi_pendeteksi_penyakit_tanaman/helpers.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '/pages/profile/edit_profile.dart';
 import '/components/page_wrap.dart';
 import '/components/input_widget.dart';
 import '/components/primary_button.dart';
+import 'auth_view.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
@@ -54,7 +56,14 @@ class Profile extends StatelessWidget {
               ],
             ),
           ),
-          PrimaryButton('Keluar', onTap: () {}),
+          PrimaryButton('Keluar', onTap: () async {
+            SharedPreferences preferences =
+                await SharedPreferences.getInstance();
+
+            preferences.clear();
+            // ignore: use_build_context_synchronously
+            Helpers().redirectPage(context, const AuthView());
+          }),
         ],
       ),
     );
